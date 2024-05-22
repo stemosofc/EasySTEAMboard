2f3a071f34898a5d537a61cd8e08a35ab47e0612
Enzo-Coutinho <enzo@stemos.com.br>
2024-05-22 09:39:22 -0300

    fix: Static class and error stat

--- lib/stemOSBoard/src/RGBLED.cpp
+++ lib/stemOSBoard/src/RGBLED.cpp
@@ -2,12 +2,6 @@
 
 Freenove_ESP32_WS2812 RGBLED::fita = Freenove_ESP32_WS2812(LEDS_COUNT, PINO, CHANNEL, TYPE_GRB);
 
-int RGBLED::AZUL[3] = {0, 0, 128};
-int RGBLED::VERMELHO[3] = {0, 0, 255};
-int RGBLED::AMARELO[3] = {255, 215, 0};
-int RGBLED::VERDE[3] = 	{0, 255, 0};
-int RGBLED::ROXO[3] = {139, 0, 139};
-
 TaskHandle_t RGBLED::LED_HANDLER = NULL;
 
 RGBLED::RGBLED() { 
@@ -20,25 +14,26 @@ void RGBLED::init() {
 }
 
 void RGBLED::ERRO() {
-  if(LED_HANDLER != nullptr) {vTaskDelete(LED_HANDLER);}
-  fita.setAllLedsColorData(VERMELHO[0], VERMELHO[1], VERMELHO[2]);
+  fita.setAllLedsColorData(Colors.VERMELHO[0], Colors.VERMELHO[1], Colors.VERMELHO[2]);
+  fita.show();
 }
 
 void RGBLED::CONFIGURE_WIFI() {
-  fita.setAllLedsColor(ROXO[0], ROXO[1], ROXO[2]);
+  fita.setAllLedsColor(Colors.ROXO[0], Colors.ROXO[1], Colors.ROXO[2]);
 }
 
 void RGBLED::NO_DS() {
   TaskHandle_t task = NULL;
-  ::xTaskCreatePinnedToCore(CW, "CONFIGURE WIFI", 1000, NULL, 2, &task, tskNO_AFFINITY);
+  ::xTaskCreatePinnedToCore(CONFIGURE_WIFI_THREAD, "CONFIGURE WIFI", 1000, NULL, 2, &task, tskNO_AFFINITY);
   LED_HANDLER = task;
 }
 
 void RGBLED::OK() {
   if(LED_HANDLER != NULL) {
     vTaskDelete(LED_HANDLER);
+    LED_HANDLER = NULL;
   }
-  fita.setAllLedsColor(VERDE[0], VERDE[1], VERDE[2]);
+  fita.setAllLedsColor(Colors.VERDE[0], Colors.VERDE[1], Colors.VERDE[2]);
 }
 
 void RGBLED::setColor(int cor[3]) {
@@ -49,13 +44,13 @@ void RGBLED::STOP() {
 
 }
 
-void RGBLED::CW(void * arg) {
+void RGBLED::CONFIGURE_WIFI_THREAD(void * arg) {
   TickType_t delay = 500 / portTICK_PERIOD_MS;
   while(1) {
-    fita.setAllLedsColorData(AZUL[0], AZUL[1], AZUL[2]);
+    fita.setAllLedsColorData(Colors.AZUL[0], Colors.AZUL[1], Colors.AZUL[2]);
     fita.show();
     vTaskDelay(delay);
-    fita.setAllLedsColor(AMARELO[0], AMARELO[1], AMARELO[2]);
+    fita.setAllLedsColor(Colors.AMARELO[0], Colors.AMARELO[1], Colors.AMARELO[2]);
     vTaskDelay(delay);
   }
 }
--- lib/stemOSBoard/src/RGBLED.h
+++ lib/stemOSBoard/src/RGBLED.h
@@ -3,27 +3,31 @@
 #include "Arduino.h"
 #include "Freenove_WS2812_Lib_for_ESP32.h"
 
+struct {
+  int AZUL[3] = {0, 0, 128};
+  int VERMELHO[3] = {255, 0, 0};
+  int AMARELO[3] = {255, 215, 0};
+  int VERDE[3] = 	{0, 255, 0};
+  int ROXO[3] = {139, 0, 139};
+} Colors;
+
+
 class RGBLED {
 public:
   RGBLED(void);
-  void configureLED();    // Aqui configuramos o led (chamada no .ino)
-  void setColor(int cor[3]);         // Quando tudo estiver correto
-  void init();
-  void ERRO();            // Sinaliza erro no código (Vermelho e preto)
-  void CONFIGURE_WIFI();  // Acesso enquanto estivermos configurando WiFi
-  void OK();   
-  void NO_DS();
+  static void configureLED();    // Aqui configuramos o led (chamada no .ino)
+  static void setColor(int cor[3]);         // Quando tudo estiver correto
+  static void init();
+  static void ERRO();            // Sinaliza erro no código (Vermelho e preto)
+  static void CONFIGURE_WIFI();  // Acesso enquanto estivermos configurando WiFi
+  static void OK();   
+  static void NO_DS();
 private:
   static const int PINO = 12;         // Pino GPIO do LED
-  static const int CHANNEL = 0;       // Canal PWM
+  static const int CHANNEL = 11;       // Canal PWM
   static const int LEDS_COUNT = 8;
-  static void CW(void * arg);
+  static void CONFIGURE_WIFI_THREAD(void * arg);
   void STOP();
-  static int AZUL[3];
-  static int VERMELHO[3];
-  static int AMARELO[3];
-  static int VERDE[3];
-  static int ROXO[3];
   static TaskHandle_t LED_HANDLER;
   static Freenove_ESP32_WS2812 fita;
 };
