273c3d03580c621a2713296032c618ae511ce9a3
Enzo-Coutinho <enzo@stemos.com.br>
2024-05-22 09:40:21 -0300

    fix: variables not used

--- lib/stemOSBoard/src/Analog.h
+++ lib/stemOSBoard/src/Analog.h
@@ -5,17 +5,14 @@
 // Classe utilizada tanto para input como output de dispositivos analógicos
 
 class Analog {
-public:
-  Analog(void);  // Construtor da classe analógica
-  int getAnalogInput();                             // Obtém uma entrada analógica do sensor acoplado
-  void setAnalogOutput(int duty);                   // Define uma saída analógica
-  static const int PORTA_ANALOGICA = 17;            // A única porta de servo na placa
-  static const int SAIDA = 0;                       // Caso for utilizar a porta analógica para saída
-  static const int ENTRADA = 1;                     // Caso for utilizar a porta analógica para entrada
-private:
-  const int CHANNEL = 10;      // Canal da porta analógica (Não configurado)
-  const int FREQUENCY = 5000;  // Frequência da porta (Não configurado)
-  const int RESOLUTION = 10;   // Resolução (Não configurado)
+  public:
+    Analog(void);  // Construtor da classe analógica
+    int getAnalogInput();                             // Obtém uma entrada analógica do sensor acoplado
+    void setAnalogOutput(int duty);                   // Define uma saída analógica
+    static const int PORTA_ANALOGICA = 17;            // A única porta de servo na placa
+    static const int SAIDA = 0;                       // Caso for utilizar a porta analógica para saída
+    static const int ENTRADA = 1;                     // Caso for utilizar a porta analógica para entrada
+  private:
 };
 
 #endif
