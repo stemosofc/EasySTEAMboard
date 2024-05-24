74d9696aa4a1db517bafddc7501e497f633044a5
Enzo-Coutinho <enzo@stemos.com.br>
2024-05-24 09:47:52 -0300

    feat: Disable ports

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
-#include "stemOSBoard.h"
+#include "Arduino.h"
 #include "UserCode.h"
-
+#include "stemWiFi.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_ipc.h>
+#include "AraraConfig.h"
+#include "esp_check.h"
+#include "esp_err.h"
+#include "driver/spi_common.h"
+#include "esp_debug_helpers.h"
 UserClass UserCode;
 stemWiFi wifi;
 TaskHandle_t handlerUser;
@@ -14,23 +22,22 @@ void loopUser(void * arg) {
   TickType_t delay = 500 / portTICK_PERIOD_MS;
   vTaskDelay(delay);
   while(1) {
-    UserCode.loop();
+      UserCode.loop();
   }
 }
 
 
 void setup() {
   Serial.begin(115200);
-
+  esp_backtrace_print(5);
   wifi.configureWiFiAP();
-
   ::xTaskCreatePinnedToCore(initUser, "InitUser", 5000, NULL, 4, &handlerUser, tskNO_AFFINITY);
   ::xTaskCreatePinnedToCore(loopUser, "LoopUser", 10000, NULL, 4, &handlerUser, tskNO_AFFINITY);
 }
 
 void DISABLE() {
-  for(int i = 0; i <= 10; i++) {
-    ledcWrite(i, 0);
+  for(int i = 0; i <= QuantPortas; i++) {
+    digitalWrite(i, LOW);
   }
 }
 
@@ -40,7 +47,7 @@ void loop() {
     vTaskResume(handlerUser);
   } else {
     vTaskSuspend(handlerUser);
-    // DISABLE();
+    DISABLE();
   }
 }
 
