20303ec6f725073611d2aba2065e625b2cc40893
Enzo-Coutinho <enzo@stemos.com.br>
2024-05-24 09:48:51 -0300

    fix: includes

--- lib/stemOSBoard/src/stemOSBoard.h
+++ lib/stemOSBoard/src/stemOSBoard.h
@@ -2,16 +2,12 @@
 #define stemOSboard_h
 #include "Arduino.h"
 #include "Motor.h"
-#include "stemWiFi.h"
 #include "Gamepad.h"
 #include "Digital.h"
 #include "ServoOS.h"
 #include "Analog.h"
-#include "RGBLED.h"
 #include "IMU.h"
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <esp_ipc.h>
+#include "PID.h"
 
 class Arara {
     private:
