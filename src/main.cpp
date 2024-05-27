3103251a9bc7f33f655eb3f3269bb868f20941d5
Enzo-Coutinho <enzo@stemos.com.br>
2024-05-27 07:20:32 -0300

    feat: log file (USB port need)

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@
 #include "AraraConfig.h"
 #include "esp_check.h"
 #include "esp_err.h"
-#include "driver/spi_common.h"
 #include "esp_debug_helpers.h"
+
 UserClass UserCode;
 stemWiFi wifi;
 TaskHandle_t handlerUser;
@@ -22,14 +22,13 @@ void loopUser(void * arg) {
   TickType_t delay = 500 / portTICK_PERIOD_MS;
   vTaskDelay(delay);
   while(1) {
-      UserCode.loop();
+    UserCode.loop();
   }
 }
 
 
 void setup() {
   Serial.begin(115200);
-  esp_backtrace_print(5);
   wifi.configureWiFiAP();
   ::xTaskCreatePinnedToCore(initUser, "InitUser", 5000, NULL, 4, &handlerUser, tskNO_AFFINITY);
   ::xTaskCreatePinnedToCore(loopUser, "LoopUser", 10000, NULL, 4, &handlerUser, tskNO_AFFINITY);
