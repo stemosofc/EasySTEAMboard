aaeb1df82db53859c5cd5aa310c2ab3e239d8a2f
Enzo-Coutinho <enzo@stemos.com.br>
2024-07-11 16:55:23 -0300

    Update ArcadeDrive.cpp

--- lib/stemOSBoard/examples/ArcadeDrive/ArcadeDrive.cpp
+++ lib/stemOSBoard/examples/ArcadeDrive/ArcadeDrive.cpp
@@ -7,30 +7,22 @@ Motor motorEsquerda(Motor::PORTA_1, Motor::REVERSE);
 Motor motorDireita(Motor::PORTA_2, Motor::FORWARD);
 
 void setup() {
-  Serial.begin(115200);
   wifi.configureWiFiAP();
 }
 
 void loop() {
-  if(estado) {
-    float y = gamepad.getLeftAxisY();
-    float turn = gamepad.getRightAxisX();
+  float y = gamepad.getLeftAxisY();
+  float turn = gamepad.getRightAxisX();
 
-    double frontLeftPower = (y + turn);
-    double backLeftPower = (y - turn);
+  double frontLeftPower = (y + turn);
+  double backLeftPower = (y - turn);
 
-    double maximum = max(abs(frontLeftPower), abs(backLeftPower));
+  double maximum = max(abs(frontLeftPower), abs(backLeftPower));
 
-    frontLeftPower /= maximum;
-    backLeftPower /= maximum;
+  frontLeftPower /= maximum;
+  backLeftPower /= maximum;
 
-    motorEsquerda.setPower(frontLeftPower);
-    motorDireita.setPower(backLeftPower);
-  }
-  if(wifi.state == "Habilitado") {
-    estado = true;
-  } else if(wifi.state == "Desabilitado") {
-    estado = false;
-  }
+  motorEsquerda.setPower(frontLeftPower);
+  motorDireita.setPower(backLeftPower);
 }
 
