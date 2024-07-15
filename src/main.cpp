d260952e9991579e1906eef346c6bb2ed821ea79
Enzo-Coutinho <enzo@stemos.com.br>
2024-07-15 07:34:20 -0300

    delete: UserCode

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,11 @@
 #include "Arduino.h"
-#include "UserCode.h"
-#include "stemWiFi.h"
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <esp_ipc.h>
-#include "AraraConfig.h"
+#include "stemOSBoard.h"
 
-UserClass UserCode;
 stemWiFi wifi;
-TaskHandle_t handlerUserInit;
-TaskHandle_t handlerUserLoop;
 
-void initUser(void * arg) {
-  UserCode.init();
-  vTaskDelete(NULL);
-}
-
-void loopUser(void * arg) {
-  TickType_t delay = 500 / portTICK_PERIOD_MS;
-  vTaskDelay(delay);
-  while(1) {
-      UserCode.loop();
-  }
-}
 
 void setup() {
-  Serial.begin(115200);
-
   wifi.configureWiFiAP();
-
-  ::xTaskCreatePinnedToCore(initUser, "InitUser", 5000, NULL, 2, &handlerUserInit, tskNO_AFFINITY);
-  ::xTaskCreatePinnedToCore(loopUser, "LoopUser", 10000, NULL, 2, &handlerUserLoop, tskNO_AFFINITY);
 }
 
 
