2c9027f11eee950a3917b97211db54e44272d1e1
Enzo-Coutinho <enzo@stemos.com.br>
2024-07-24 08:12:43 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "stemOSBoard.h"
 
+Gamepad gamepad;
 
 void setup() {
   wifi.configureWiFiAP();
@@ -7,7 +8,10 @@ void setup() {
 
 
 void loop() {
-
+  motor1.setPower(gamepad.getLeftAxisY());
+  motor2.setPower(gamepad.getLeftAxisY());
+  motor3.setPower(gamepad.getLeftAxisY());
+  motor4.setPower(gamepad.getLeftAxisY());
 }
 
 
