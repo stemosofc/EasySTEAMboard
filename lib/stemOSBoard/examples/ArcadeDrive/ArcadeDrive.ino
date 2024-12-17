92e1c0bedcaed72f81c79aa190d845ea7e3177e1
Enzo-Coutinho <enzo@stemos.com.br>
2024-12-17 16:11:26 -0300

    update example code

--- lib/stemOSBoard/examples/ArcadeDrive/ArcadeDrive.ino
+++ lib/stemOSBoard/examples/ArcadeDrive/ArcadeDrive.ino
@@ -1,22 +1,24 @@
-#include "Arara.h"
+#include "EasySTEAM.h"
 
 void setup() {
-  Arara.start();
+  easySTEAM.start();
+  Serial.begin(115200);
 }
 
 void loop() {
-  float y = gamepad.getLeftAxisY();
-  float turn = gamepad.getRightAxisX();
+    float y = gamepad.getLeftAxisY();
+    float turn = gamepad.getRightAxisX();
 
-  double frontLeftPower = (y + turn);
-  double backLeftPower = (y - turn);
+    double frontLeftPower = (y + turn);
+    double backLeftPower = (y - turn);
 
-  double maximum = max(abs(frontLeftPower), abs(backLeftPower));
-
-  frontLeftPower /= maximum;
-  backLeftPower /= maximum;
-
-  motor1.setPower(frontLeftPower);
-  motor2.setPower(backLeftPower);
+    double maximum = max(abs(frontLeftPower), abs(backLeftPower));
+    if(maximum > 1) {
+        frontLeftPower /= maximum;
+        backLeftPower /= maximum;
+    }
+    
+    motor1.setPower(-frontLeftPower);
+    motor2.setPower(backLeftPower);
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-#include "Arara.h"
+#include "EasySTEAM.h"
 
 void setup() {
-
+  easySTEAM.start();
 }
 
 void loop() {
