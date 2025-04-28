52104f051b4b8ace20ab831f9ef49545a7eb6b15
Enzo-Coutinho <enzo@stemos.com.br>
2025-04-28 15:50:40 -0300

    save main file

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,8 @@
 
 void setup() {
   Serial.begin(115200);
-  pinPoint.start();
+  easySTEAM.start();
 }
 void loop() {
-  pinPoint.update();
-  //Serial.printf("Device status: %d\n", pinPoint.getDeviceStatus());
-  Serial.printf("Yaw: %f\n", pinPoint.getYaw());
-  delay(50);
+
 }
